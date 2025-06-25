1c55bf39a16b0dd1f1e7777305d8626c1ce680e9
Ho Ka Lok <132459560+Mr-RickyHo@users.noreply.github.com>
2025-06-25 09:12:57 +0800

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,7 @@
+A weather station records temperatures (in Celsius) every hour for a day (24 readings). The temperatures are stored in a list called temperatures.
+
+Using a for loop, write code to print all temperatures above 30°C.
+Using a while loop, find and print the index of the first temperature below 10°C. If there is no such temperature, print "No temperature below 10°C found."
+Example data:
+
+temperatures = [28, 29, 31, 33, 27, 25, 22, 21, 19, 15, 13, 12, 9, 8, 10, 11, 13, 16, 18, 20, 22, 24, 26, 28]
